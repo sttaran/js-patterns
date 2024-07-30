c0346bdfceda195ef7fea5aa0666a9da03f50abc
staran <staran@griddynamics.com>
2024-07-30 23:26:58 +0300

    staran// factory

---
+++ creational/factory/practice.js
@@ -0,0 +1,18 @@
+class Person
+{
+    constructor(id, name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class PersonFactory
+{
+    createPerson(name)
+    {
+        return new Person(PersonFactory.id++,name)
+    }
+}
+
+PersonFactory.id = 0
